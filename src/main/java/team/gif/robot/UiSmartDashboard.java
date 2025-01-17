70032ec5ce71c0f3817174d9b878d04cfc7d0b2d
Caleb Herring <calebsethherring+github@gmail.com>
2025-01-16 21:15:45 -0600

    Code review complete

--- src/main/java/team/gif/robot/UiSmartDashboard.java
+++ src/main/java/team/gif/robot/UiSmartDashboard.java
@@ -1,33 +1,35 @@
 package team.gif.robot;
 
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
 public class UiSmartDashboard {
+
+    private NetworkTable networkTable;
+    private NetworkTableEntry motorTempEntry;
 
     /**
      *  Widgets (e.g. gyro),
      *  buttons (e.g. SmartDashboard.putData("Reset", new ResetHeading()); ),
      *  and Chooser options (e.g. auto mode)
      *
-     *  Placed on a dashboard tab
+     *  Placed in 2338-dashboard network table
      *  After SmartDashboard loads for the first time, move items from network table onto Dashboard tab
      *  and save file as "YYYY shuffleboard layout.json"
      */
     public UiSmartDashboard() {
-        ShuffleboardTab tab = Shuffleboard.getTab("SmartDashboard");
-        tab.addBoolean("Motor Temp", Robot.diagnostics::getAnyMotorTempHot);
+        networkTable = NetworkTableInstance.getDefault().getTable("2338-dashboard");
+        motorTempEntry = networkTable.getEntry("Motor Temp");
     }
 
     /**
      * Values which are updated periodically should be placed here
      *
      * Convenient way to format a number is to use putString w/ format:
-     *     SmartDashboard.putString("Elevator", String.format("%11.2f", Elevator.getPosition()));
+     *     elevatorPosEntry.setString(String.format("%11.2f", Elevator.getPosition());
      */
     public void updateUI() {
-
+        motorTempEntry.setBoolean(Robot.diagnostics.getAnyMotorTempHot());
     }
 }
