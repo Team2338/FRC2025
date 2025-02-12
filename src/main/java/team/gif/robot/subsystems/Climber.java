49cfc60dbe32a9164b2818799bd2807dfc873c46
Shruti Vavilala <svavilala22@gmail.com>
2025-02-11 19:30:43 -0600

    fixed lint

--- src/main/java/team/gif/robot/subsystems/Climber.java
+++ src/main/java/team/gif/robot/subsystems/Climber.java
@@ -8,7 +8,6 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.SoftwareLimitSwitchConfigs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
-import com.ctre.phoenix6.controls.PositionDutyCycle;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
