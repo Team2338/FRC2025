dc8d479e9d70e804b391f0b3dd6fd12a33285305
Caleb Herring <calebsethherring+github@gmail.com>
2025-02-17 14:54:54 -0600

    remove unused imports

--- src/main/java/team/gif/robot/subsystems/drivers/swerve/TalonFXDriveMotor.java
+++ src/main/java/team/gif/robot/subsystems/drivers/swerve/TalonFXDriveMotor.java
@@ -1,8 +1,6 @@
 package team.gif.robot.subsystems.drivers.swerve;
 
-import com.ctre.phoenix6.configs.FeedbackConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.configs.TalonFXConfigurator;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
