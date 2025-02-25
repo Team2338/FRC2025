22c564dfb2996d472f4a805ade517669a7e9f3ca
Shruti Vavilala <svavilala22@gmail.com>
2025-02-24 19:29:27 -0600

    drive away slowly - .3 seconds

---
+++ src/main/java/team/gif/robot/commands/ShortDriveAway.java
@@ -0,0 +1,41 @@
+package team.gif.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import team.gif.lib.drivePace;
+import team.gif.robot.Robot;
+
+public class ShortDriveAway extends Command {
+    private int counter;
+
+    public ShortDriveAway() {
+        super();
+        addRequirements(Robot.swerveDrive); // uncomment
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        Robot.swerveDrive.setDrivePace(drivePace.COAST_RR);
+    }
+
+    // Called every time the scheduler runs (~20ms) while the command is scheduled
+    @Override
+    public void execute() {
+        if (counter <= 15) {
+            Robot.swerveDrive.drive(0, -.15, 0.0);
+        }
+        counter++;
+    }
+
+    // Return true when the command should end, false if it should continue. Runs every ~20ms.
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    // Called when the command ends or is interrupted.
+    @Override
+    public void end(boolean interrupted) {
+        Robot.swerveDrive.setDrivePace(drivePace.COAST_FR);
+    }
+}
