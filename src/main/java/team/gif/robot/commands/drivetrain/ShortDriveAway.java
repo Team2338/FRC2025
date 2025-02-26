f1c67b618b47ca933fc7010ad9c211237357e5e6
Shruti Vavilala <svavilala22@gmail.com>
2025-02-25 18:14:23 -0600

    Restrict drive away to level 4

--- src/main/java/team/gif/robot/commands/drivetrain/ShortDriveAway.java
+++ src/main/java/team/gif/robot/commands/drivetrain/ShortDriveAway.java
@@ -2,6 +2,7 @@ package team.gif.robot.commands.drivetrain;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import team.gif.lib.drivePace;
+import team.gif.robot.Constants;
 import team.gif.robot.Robot;
 
 public class ShortDriveAway extends Command {
@@ -9,7 +10,7 @@ public class ShortDriveAway extends Command {
 
     public ShortDriveAway() {
         super();
-        addRequirements(Robot.swerveDrive); // uncomment
+//        addRequirements(Robot.swerveDrive); // uncomment
     }
 
     // Called when the command is initially scheduled.
@@ -22,8 +23,14 @@ public class ShortDriveAway extends Command {
     // Called every time the scheduler runs (~20ms) while the command is scheduled
     @Override
     public void execute() {
-        Robot.swerveDrive.drive(-0.30, 0.0, 0.0);
-        counter++;
+        // only move if elevator target is level 4
+        // otherwise, end the command by setting the counter very high
+        if( Robot.elevator.getTargetPosition() == Constants.Elevator.LEVEL_4_POSITION) {
+            Robot.swerveDrive.drive(-0.30, 0.0, 0.0);
+            counter++;
+        } else {
+            counter = 5000;
+        }
     }
 
     // Return true when the command should end, false if it should continue. Runs every ~20ms.
