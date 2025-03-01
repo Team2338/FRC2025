0f5f6b5369902b0aa93a2d0faa385cb71e0dc8c3
Caleb Herring <calebsethherring+github@gmail.com>
2025-03-01 08:20:15 -0600

    remove unused import

--- src/main/java/team/gif/robot/commands/shooter/AutoDriveAndShoot.java
+++ src/main/java/team/gif/robot/commands/shooter/AutoDriveAndShoot.java
@@ -1,7 +1,6 @@
 package team.gif.robot.commands.shooter;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import team.gif.lib.drivePace;
