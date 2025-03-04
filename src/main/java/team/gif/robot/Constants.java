2f2f37401fe910f24a23abbd66e00b5a140aa4a3
Bill Knopf <billknopfjr@yahoo.com>
2025-03-04 01:39:20 -0600

    Effectively piston out immediately but leave variable code intact

--- src/main/java/team/gif/robot/Constants.java
+++ src/main/java/team/gif/robot/Constants.java
@@ -256,7 +256,7 @@ public final class Constants {
         public static final double CLIMB_PERCENT = 0.75;
         public static final double FORWARD_SOFT_LIMIT = 354; //332;
         public static final double REVERSE_SOFT_LIMIT = 175;
-        public static final double PISTON_DEPLOY_POS = FORWARD_SOFT_LIMIT - 10;
+        public static final double PISTON_DEPLOY_POS = FORWARD_SOFT_LIMIT;
         public static final double DRIVE_SPEED_MPS = 0.1; //Speed to drive while climbing in meters per second
     }
 
